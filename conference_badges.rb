e3c9f205ec79846e24490e773289492e4df7f219
remote-torrent-6747 <abbyc@sky.com>
2020-12-08 21:56:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(attendees)
   return room_assignments
 end
 
-def printer(attendees)
+def printer(attendees) 
   attendees.each do |speaker|
     puts "Hello, my name is #{speaker}."
   end
